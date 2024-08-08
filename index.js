21904fa9382a741f1858acd0b5f09c2bff28b6ca
ipi-dot <i.piso1998@gmail.com>
2024-08-08 14:00:55 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import './styles/App.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <ChakraProvider>
+    <Router>
+      <App />
+    </Router>
+  </ChakraProvider>
+);
